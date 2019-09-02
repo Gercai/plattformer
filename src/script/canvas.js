47c41b5528bfa8fe1b99428c25a8ea6a0a769aea
geraldjän <solidcaius@gmail.com>
2019-09-02 16:10:08 +0200

    kreis

--- src/script/canvas.js
+++ src/script/canvas.js
@@ -1,9 +1,13 @@
 // ctx steht für Context!
 
 function draw(){
-    var canvas = document.getElementById('tutorial');
+    var canvas = document.getElementById('myCanvas');
     if(canvas.getContext){
         var ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+        ctx.stroke();
+
     }
 }
 
