916e3c0ce2227d796cc685318c4eefc3a694534d
geraldjän <solidcaius@gmail.com>
2019-09-17 15:56:04 +0200

    testing further

--- player.js
+++ player.js
@@ -12,50 +12,58 @@
 var full = 1;
 var canvas = document.getElementById("myCanvas");
 
+window.addEventListener( "keydown", gameLoop, true);
 
-
-
-window.addEventListener( "keydown", DoKeyDown, true);
+draw();
 
 let xAxis = 125;
 let yAxis = 55;
 let xMove = 0;
 let yMove = 0;
 
-
-
-function DoKeyDown(e) {
-    
-
-    if(e.keyCode == 39){
-        xMove = 5;
-        moveCircle();
-     
-     } else if (e.keyCode == 37){
-     
-         xMove = -5;
-         moveCircle();
-    
-     } else if (e.keyCode == 40) {
-     
-         yMove = 5;
-         moveCircle();
-    
-     } else if (e.keyCode == 38){
-         yMove = -5;
-         moveCircle();
-    
-     }
-     else{
-        alert(e.keyCode);
-     
-     }
-     
-
-}
-
-
 var ctx = canvas.getContext("2d");
+
+let keyState = {};
+
+window.addEventListener('keydown',function(e){
+    keyState[e.keyCode || e.which] = true;
+},true);
+window.addEventListener('keyup',function(e){
+    keyState[e.keyCode || e.which] = false;
+},true);
+
+
+function gameLoop() {
+    if (keyState[37] || keyState[65]){
+        xMove -= 1;
+        moveCircle();
+    }    
+    if (keyState[39] || keyState[68]){
+        xMove += 1;
+        moveCircle();
+    }
+    if (keyState[40]){
+        yMove += 1;
+        moveCircle();
+    }
+    if (keyState[38]){
+        yMove -= 1;
+        moveCircle();
+    }
+
+    // redraw/reposition your object here
+    // also redraw/animate any objects not controlled by the user
+
+    setTimeout(gameLoop, 10);
+
+}    
+
+ gameLoop();
+
+player1 = {
+
+};
+
 
 function createCircle(){
 ctx.save();
@@ -77,7 +85,7 @@ console.log(ctx);
 
 function moveCircle(){
 
-ctx.clearRect(0,0,600,600);
+ctx.clearRect(0,0,1400,1000);
 
 
 xAxis = xAxis +xMove;
@@ -107,3 +115,32 @@ function empty(){
     kreis();
     }
 }
+
+for (var column = 0; column < map.columns; column++) {
+    for (var row = 0; row < map.rows; row++) {
+      var tile = map.getTile(column, row);
+      var x = column * map.tileSize;
+      var y = row * map.tileSize;
+      drawTile(tile, x, y);
+    }
+  }
+
+
+
+ctx.fillStyle = 'orange';
+ctx.fillStyle = '#FFA500';
+ctx.fillStyle = 'rgb(255, 165, 0)';
+ctx.fillStyle = 'rgba(255, 165, 0, 1)';
+
+function draw() {
+    var ctx = document.getElementById('myCanvas').getContext('2d');
+    for (var i = 0; i < 70; i++) {
+      for (var j = 0; j < 70; j++) {
+        ctx.fillStyle = 'rgb(' + Math.floor(255 - 5.5 * i) + ', ' +
+                         Math.floor(255 - 5.5 * j) + ', 0)';
+        ctx.fillRect(j * 25, i * 25, 25, 25);
+      } 
+    }
+  }
+
+ 
